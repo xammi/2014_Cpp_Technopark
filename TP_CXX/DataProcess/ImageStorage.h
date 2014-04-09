f993884db12ca05168ffb1e0572ba24b3d3c82ac
xammi-1 <kmg.xammi.1@gmail.com>
2014-04-09 13:25:48 +0400

    New packages: NetTutors and DataProcess

---
+++ TP_CXX/DataProcess/ImageStorage.h
@@ -0,0 +1,19 @@
+#ifndef IMAGESTORAGE_H
+#define IMAGESTORAGE_H
+
+#include "AbstractStorage.h"
+
+namespace DataProcess {
+
+//-------------------------------------------------------------------------------------------------
+class ImageStorage : public AbstractStorage {
+    Q_OBJECT
+
+public:
+    ImageStorage();
+
+private:
+};
+//-------------------------------------------------------------------------------------------------
+} // namespace DataProcess
+#endif // IMAGESTORAGE_H
---
+++ TP_CXX/NetTutors/BackPropTutor.h
@@ -0,0 +1,17 @@
+#ifndef BACKPROPTUTOR_H
+#define BACKPROPTUTOR_H
+
+#include "AbstractTutor.h"
+
+namespace NetTutors {
+
+//-------------------------------------------------------------------------------------------------
+class BackPropTutor : public AbstractTutor {
+    Q_OBJECT
+public:
+    BackPropTutor();
+    
+};
+//-------------------------------------------------------------------------------------------------
+} // namespace NetTutors
+#endif // BACKPROPTUTOR_H
